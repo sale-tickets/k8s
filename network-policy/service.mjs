38dd6a86d5fe0dc3a2d1fc01a46d136555dd9780
ndhung <nguyenduyhung04092004@gmail.com>
2025-10-01 14:23:00 +0700

    [Init] init k8s

---
+++ network-policy/service.mjs
@@ -0,0 +1,36 @@
+const agrs = [...process.argv];
+const option = agrs.slice(3, agrs.length);
+
+const apply = async () => {
+    try {
+        const buildHelm = await $`helm upgrade --install network-policy -f k8s/values.yaml -n dev k8s`;
+        console.log(buildHelm.stdout)
+    } catch (error) {
+        console.log("ERROR: ", error);
+    }
+}
+
+const stop = async () => {
+    try {
+        const stopHelm = await $`helm uninstall network-policy -n dev`;
+        console.log(stopHelm.stdout)
+    } catch (error) {
+        console.log("ERROR: ", error);
+    }
+}
+
+const run = (option) => {
+    switch (option) {
+        case "apply":
+            apply();
+            break;
+        case "stop":
+            stop();
+            break;
+        default:
+            console.log("zx service.mjs apply|stop");
+            break;
+    }
+}
+
+run(option.length === 0 ? "" : option[0]);
